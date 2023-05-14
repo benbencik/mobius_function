6ce0af95894c233494e9a5431dd1746568d91f8e
benbencik <bencik42@gmail.com>
2023-05-14 20:52:56 +0200

    recursive function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// Mobuis function on a poset
+
+
 fn main() {
-    println!("Hello, world!");
+    // let poset: [i32; 5] = [1, 2, 3, 4, 5];
+    let l: i32 = 0;
+    let r: i32 = 4;
+    mobius_function(l, r, 0);
+
+}
+
+
+
+fn mobius_function(l: i32, r: i32, val: i32) -> i32{
+    if l == r {
+        return 1;
+    }
+    else if l < r{
+        for i in l..r {
+            // let val = val + mobius_function(poset, i, r, val);
+            println!("{}", i);
+        }
+        return val;
+    }
+    else {
+        return 0;
+    }
 }
