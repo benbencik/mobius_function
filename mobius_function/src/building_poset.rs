0b8c2c72705e1ad0544ca0885906193f520220a5
benbencik <bencik42@gmail.com>
2023-07-23 17:37:21 +0200

    building poset as a hashmap

---
+++ mobius_function/src/building_poset.rs
@@ -0,0 +1,58 @@
+use std::collections::{LinkedList, HashMap};
+use crate::is_contained::is_contained;
+
+pub fn build_poset(start: &Vec<u8>, goal: &Vec<u8>) -> HashMap<Vec<u8>, Vec<Vec<u8>>>{
+    /*
+    Poset is built from the goal permutation
+    Hashmap contains links between the multipermutations 
+     */
+
+    let mut queue: LinkedList<Vec<u8>> = LinkedList::new();
+    let mut mperms: HashMap<Vec<u8>, Vec<Vec<u8>>> = HashMap::new();
+
+    queue.push_back(start.clone());
+    mperms.insert(start.clone(), Vec::new());
+
+    while queue.len() > 0 {
+        let current_mperm = queue.pop_front().unwrap();
+        
+        if current_mperm == *goal {
+            println!("Found goal!");
+            break;
+        }
+
+        // decremetning elements by one
+        for i in 0..current_mperm.len() {
+            let mut new_mperm = current_mperm.clone();
+            if new_mperm[i] > 1 {
+                new_mperm[i] -= 1;
+                if is_contained(&new_mperm, &current_mperm){
+                    if !mperms.contains_key(&new_mperm) {
+                        println!("Adding {:?} to queue", new_mperm);
+                        queue.push_back(new_mperm.clone());
+                        mperms.insert(new_mperm.clone(), Vec::new());
+                    }
+                    mperms.get_mut(&current_mperm).unwrap().push(new_mperm.clone());
+                }
+            }
+        }
+
+        // removing single elements
+        if current_mperm.len() > goal.len() {
+            for i in 0..current_mperm.len() {
+                let mut new_mperm = current_mperm.clone();
+                new_mperm.remove(i);
+                if is_contained(&new_mperm, &current_mperm){
+                    if !mperms.contains_key(&new_mperm) {
+                        queue.push_back(new_mperm.clone());
+                        mperms.insert(new_mperm.clone(), Vec::new());
+                    }
+                    mperms.get_mut(&current_mperm).unwrap().push(new_mperm.clone());
+                }
+            }
+        }
+    }
+
+    return mperms;
+
+}
