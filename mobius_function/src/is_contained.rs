e9c09bd8e28e695518b6858df34a4a13531d84ae
benbencik <bencik42@gmail.com>
2023-07-23 17:05:44 +0200

    added condition

--- mobius_function/src/is_contained.rs
+++ mobius_function/src/is_contained.rs
@@ -1,4 +1,4 @@
-pub fn is_contained(a: &[i32], b: &[i32]) -> bool {
+pub fn is_contained(a: &Vec<u8>, b: &Vec<u8>) -> bool {
     /* 
     Naive implementation of a function that checks if 
     multipermutation "a" is contained in "b" 
@@ -12,25 +12,38 @@ pub fn is_contained(a: &[i32], b: &[i32]) -> bool {
     if b.len() < a.len() { return false; }
     if (b.len() - a.len()) > 1 { return false; }
 
-
-
-
-    // choose omitted element
-    for i in 0..n {
+    if a.len() == b.len(){
         // itterate through all positions
         valid = true;
-        for j in 0..n-2 {
-            for k in j+1..n-1 {
-                let elem1 = if j < i {j} else {j+1};
-                let elem2 = if k < i {k} else {k+1};
-                if (a[j] == a[k] && b[elem1] != b[elem2]) || (a[j] <= a[k] && b[elem1] > b[elem2]) {
+        for j in 0..n-1 {
+            for k in j+1..n {
+                if (a[j] == a[k] && b[j] != b[k]) || (a[j] <= a[k] && b[j] > b[k]) {
                     valid = false;
                     break;
                 }
             }
-            if !valid { break; }
         }
-        if valid { return true; }
+        return valid;
+    }
+    else{
+        // choose omitted element
+        for i in 0..n {
+            // itterate through all positions
+            valid = true;
+            for j in 0..n-2 {
+                for k in j+1..n-1 {
+                    let elem1 = if j < i {j} else {j+1};
+                    let elem2 = if k < i {k} else {k+1};
+                    if (a[j] == a[k] && b[elem1] != b[elem2]) || (a[j] <= a[k] && b[elem1] > b[elem2]) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if !valid { break; }
+            }
+            if valid { return true; }
+        }
     }
     return false;
+
 }
