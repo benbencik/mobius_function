cc53adcc37cd84125245d01db6d5b530c805906a
benbencik <bencik42@gmail.com>
2023-07-23 17:05:30 +0200

    changed signature to references

--- mobius_function/tests/is_contained_test.rs
+++ mobius_function/tests/is_contained_test.rs
@@ -2,79 +2,56 @@ use mobius_funciton::is_contained;
 
 #[test]
 fn test_01() {
-    let x = [1, 2, 3];
-    let y = [1, 2, 3, 4];
-    assert_eq!(is_contained(&x, &y), true);
+    assert_eq!(is_contained(&vec![1, 2, 3] , &vec![1, 2, 3, 4]), true);
 }
 
 #[test]
 fn test_02() {
-    let x = [1, 2, 3, 4];
-    let y = [1, 2, 3, 4];
-    assert_eq!(is_contained(&x, &y), true);
+    assert_eq!(is_contained(&vec![1, 2, 3, 4], &vec![1, 2, 3, 4]), true);
 }
 
 #[test]
 fn test_03() {
-    let x = [1, 2, 2];
-    let y = [1, 2, 3, 4];
-    assert_eq!(is_contained(&x, &y), false);
+    assert_eq!(is_contained(&vec![1, 2, 2], &vec![1, 2, 3, 4]), false);
 }
 
 #[test]
 fn test_04() {
-    let x = [1, 1, 1];
-    let y = [1, 2, 3, 4];
-    assert_eq!(is_contained(&x, &y), false);
+    assert_eq!(is_contained(&vec![1, 1, 1], &vec![1, 2, 3, 4]), false);
 }
 
 #[test]
 fn test_05() {
-    let x = [1, 2, 2];
-    let y = [1, 2, 3, 3];
-    assert_eq!(is_contained(&x, &y), true);
+    assert_eq!(is_contained(&vec![1, 2, 2] , &vec![1, 2, 3, 3]), true);
 }
 
 #[test]
 fn test_06() {
-    let x = [4, 4, 4];
-    let y = [4, 3, 2, 1];
-    assert_eq!(is_contained(&x, &y), false);
+    assert_eq!(is_contained(&vec![4, 4, 4] , &vec![4, 3, 2, 1]), false);
 }
 
 #[test]
 fn test_07() {
-    let x = [4, 3, 2];
-    let y = [4, 3, 2, 1];
-    assert_eq!(is_contained(&x, &y), true);
+    assert_eq!(is_contained(&vec![4, 3, 2], &vec![4, 3, 2, 1]), true);
 }
 
 #[test]
 fn test_08() {
-    let x = [3, 2, 1];
-    let y = [7, 6, 5, 4];
-    assert_eq!(is_contained(&x, &y), true);
+    assert_eq!(is_contained(&vec![3, 2, 1], &vec![7, 6, 5, 4]), true);
 }
 
 #[test]
 fn test_09() {
-    let x = [7, 5, 6];
-    let y = [7, 6, 5, 4];
-    assert_eq!(is_contained(&x, &y), false);
+    assert_eq!(is_contained(&vec![7, 5, 6], &vec![7, 6, 5, 4]), false);
 }
 
 #[test]
 fn test_10() {
-    // 
-    let x = [1];
-    let y = [1, 2, 3];
-    assert_eq!(is_contained(&x, &y), false);
+    assert_eq!(is_contained(&vec![1], &vec![1, 2, 3]), false);
 }
 
 #[test]
 fn test_11() {
-    let x = [1, 2, 3];
-    let y = [1, 2];
-    assert_eq!(is_contained(&x, &y), false);
+    assert_eq!(is_contained(&vec![1, 2, 3], &vec![1, 2]), false);
 }
 
