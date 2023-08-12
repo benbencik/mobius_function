9839fe692cc137d3fe8226409f0e34b3b1140f19
benbencik <bencik42@gmail.com>
2023-08-12 15:59:41 +0200

    move unit tests

--- mobius_function/src/is_contained.rs
+++ mobius_function/src/is_contained.rs
@@ -57,3 +57,62 @@ pub fn is_contained(a: &Vec<u8>, b: &Vec<u8>) -> bool {
     }
     return false;
 }
+
+mod tests {
+    use super::is_contained;
+
+    #[test]
+    fn test_01() {
+        assert_eq!(is_contained(&vec![1, 2, 3] , &vec![1, 2, 3, 4]), true);
+    }
+
+    #[test]
+    fn test_02() {
+        assert_eq!(is_contained(&vec![1, 2, 3, 4], &vec![1, 2, 3, 4]), true);
+    }
+
+    #[test]
+    fn test_03() {
+        assert_eq!(is_contained(&vec![1, 2, 2], &vec![1, 2, 3, 4]), false);
+    }
+
+    #[test]
+    fn test_04() {
+        assert_eq!(is_contained(&vec![1, 1, 1], &vec![1, 2, 3, 4]), false);
+    }
+
+    #[test]
+    fn test_05() {
+        assert_eq!(is_contained(&vec![1, 2, 2] , &vec![1, 2, 3, 3]), true);
+    }
+
+    #[test]
+    fn test_06() {
+        assert_eq!(is_contained(&vec![4, 4, 4] , &vec![4, 3, 2, 1]), false);
+    }
+
+    #[test]
+    fn test_07() {
+        assert_eq!(is_contained(&vec![4, 3, 2], &vec![4, 3, 2, 1]), true);
+    }
+
+    #[test]
+    fn test_08() {
+        assert_eq!(is_contained(&vec![3, 2, 1], &vec![7, 6, 5, 4]), true);
+    }
+
+    #[test]
+    fn test_09() {
+        assert_eq!(is_contained(&vec![7, 5, 6], &vec![7, 6, 5, 4]), false);
+    }
+
+    #[test]
+    fn test_10() {
+        assert_eq!(is_contained(&vec![1], &vec![1, 2, 3]), false);
+    }
+
+    #[test]
+    fn test_11() {
+        assert_eq!(is_contained(&vec![1, 2, 3], &vec![1, 2]), false);
+    }
+}
--- mobius_function/tests/dummy_test.rs
+++
@@ -1,5 +0,0 @@
-#[test]
-fn test_addition() {
-    let result = 2 + 2;
-    assert_eq!(result, 4);
-}
