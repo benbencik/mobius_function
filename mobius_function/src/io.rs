e7b6867a2375710f2f2bcac5e299293867c9b13d
benbencik <bencik42@gmail.com>
2023-08-15 08:29:09 +0200

    added module for loading test files

---
+++ mobius_function/src/io.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub fn load_tests(path: String) -> Result<(Vec<Vec<u8>>, Vec<i32>), Box<dyn std::error::Error>> {
+    println!("{}", path);
+    let path = Path::new(&path);
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut data_vec: Vec<Vec<u8>> = Vec::new();
+    let mut result: Vec<i32> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let mut numbers: Vec<i32> = line
+            .split_whitespace()
+            .map(|num_str| num_str.parse::<i32>().unwrap())
+            .collect();
+
+        let last_num = numbers.pop().unwrap();
+        result.push(last_num);
+
+        let mut unums: Vec<u8> = Vec::new();
+        for x in numbers {
+            unums.push(x as u8);
+        }
+        data_vec.push(unums);
+    }
+
+    return Ok((data_vec, result));
+}
