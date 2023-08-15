9e7d1a0c7a373d4fdd2a6872e2ba14627d6bdce2
benbencik <bencik42@gmail.com>
2023-08-15 08:44:15 +0200

    added testing from files

--- mobius_function/src/main.rs
+++ mobius_function/src/main.rs
@@ -1,4 +1,5 @@
 mod building_poset;
+mod io;
 mod is_contained;
 mod mobius_func;
 mod multipermutation;
@@ -6,12 +7,59 @@ mod multipermutation;
 use multipermutation::Mperm;
 
 fn main() {
-    let x: Mperm = Mperm::new(vec![1, 2], 2);
-    // let y: Mperm = Mperm::new(vec![1, 2, 3, 3], 3); // shpuld be 1
-    let y: Mperm = Mperm::new(vec![1, 2, 3, 3, 4], 4);  // should be -1
+    let bottom: Mperm = Mperm::new(vec![1]);
+    let top: Mperm = Mperm::new(vec![1, 2, 3, 5, 4]);
 
-    let (downward_links, upward_links) = building_poset::build_poset(&x, &y);
-    let result = mobius_func::naive(&x, &y, &downward_links, &upward_links);
-    println!("{}", result);
+    let (downward_links, upward_links) = building_poset::build_poset(&bottom, &top);
+    let result = mobius_func::naive(&bottom, &top, &downward_links, &upward_links);
+    println!("Result of μ({:?}, {:?}) is: {}", bottom, top, result);
+
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    fn run_test_file(path: String) {
+        let res = io::load_tests(path);
+        match res {
+            Ok((tests, result)) => {
+                for (test, res) in tests.iter().zip(result.iter()) {
+                    let bottom = Mperm::new(vec![1]);
+                    let c = test.clone();
+                    let top = Mperm::new(c);
+                    let (downward_links, upward_links) = building_poset::build_poset(&bottom, &top);
+                    let result = mobius_func::naive(&bottom, &top, &downward_links, &upward_links);
+                    assert_eq!(result, *res);
+                }
+            }
+            Err(e) => println!("{}", e),
+        }
+    }
+
+    #[test]
+    fn test5() {
+        run_test_file(String::from("../test_files/all5.txt"));
+    }
+
+    #[test]
+    fn test6() {
+        run_test_file(String::from("../test_files/all5.txt"));
+    }
+
+    #[test]
+    fn test7() {
+        run_test_file(String::from("../test_files/all5.txt"));
+    }
+
+    #[test]
+    fn test8() {
+        run_test_file(String::from("../test_files/all5.txt"));
+    }
+
+    #[test]
+    fn test9() {
+        run_test_file(String::from("../test_files/all5.txt"));
+    }
+}
