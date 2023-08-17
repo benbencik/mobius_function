595bdae85fe6ce38e8cf1160fd5808becb04ce16
benbencik <bencik42@gmail.com>
2023-08-17 11:04:10 +0200

    add central module file

---
+++ mobius_function/src/lib.rs
@@ -0,0 +1,11 @@
+/// loading test files
+pub mod io;
+
+/// building poset
+pub mod building_poset;
+
+/// calculating mobius function
+pub mod mobius_func;
+
+/// multipermutation data structure
+pub mod multipermutation;
